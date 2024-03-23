ff31631f5be878c48f417d1dc42ac66366c079cf
Abdumalikov Sardor <sardorlearnjs@gmail.com>
2024-03-24 01:33:17 +0700

    ready file for check)

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,6 +1,7 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+
 import psycopg2
-import os, csv
 
 conn = psycopg2.connect(
     host="localhost",
@@ -9,23 +10,29 @@ conn = psycopg2.connect(
     password="12345"
 )
 
-#Подключение к базе данных
+# Подключение к базе данных
 try:
-    for filename in os.listdir('north_data'):
-        with psycopg2.connect(
+    with psycopg2.connect(
             host="localhost",
             database="north",
             user="postgres",
             password="12345"
-        ) as conn:
-
-            with conn.cursor() as cur:
-                with open(f'north_data\\{filename}') as file:
-                    header = next(file)
-                    reader = csv.reader(file)
-                    for row in reader:
-                        cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", row)
+    ) as conn:
+        with conn.cursor() as cur:
+            with open(f'north_data/employees_data.csv') as file:
+                header = next(file)
+                reader = csv.reader(file)
+                for row in reader:
+                    cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", row)
+            with open(f'north_data/customers_data.csv') as file:
+                header = next(file)
+                reader = csv.reader(file)
+                for row in reader:
+                    cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", row)
+            with open(f'north_data/orders_data.csv') as file:
+                header = next(file)
+                reader = csv.reader(file)
+                for row in reader:
+                    cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", row)
 finally:
     conn.close()
-
-
