adac5498e622e2bfe2798775ab36ca8818711468
Abdumalikov Sardor <sardorlearnjs@gmail.com>
2024-03-23 18:47:48 +0700

    test

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,31 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import os, csv
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="12345"
+)
+
+#Подключение к базе данных
+try:
+    for filename in os.listdir('north_data'):
+        with psycopg2.connect(
+            host="localhost",
+            database="north",
+            user="postgres",
+            password="12345"
+        ) as conn:
+
+            with conn.cursor() as cur:
+                with open(f'north_data\\{filename}') as file:
+                    header = next(file)
+                    reader = csv.reader(file)
+                    for row in reader:
+                        cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", row)
+finally:
+    conn.close()
+
+
